71f6df475b0fffc0fc12eb5a7dda13cb3230fd11
rdt.project <rdt.project@gmail.com>
2018-02-13 22:57:02 +0100

    updated tests

--- src/test/java/pl/pietro/XmlPoligon/AppTest.java
+++ src/test/java/pl/pietro/XmlPoligon/AppTest.java
@@ -55,30 +55,4 @@ public class AppTest extends TestCase {
 		assertTrue(true);
 	}
 
-	public void testAppointmentFailValidation() throws ParserConfigurationException, SAXException, IOException {
-
-		// parse an XML document into a DOM tree
-		DocumentBuilder parser = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-		Document document = parser.parse(new File("src/test/resources/Appointment.xml"));
-
-		// create a SchemaFactory capable of understanding WXS schemas
-		SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-
-		// load a WXS schema, represented by a Schema instance
-		Source schemaFile = new StreamSource(new File("src/test/resources/Appointment.xsd"));
-		Schema schema = factory.newSchema(schemaFile);
-
-		// create a Validator instance, which can be used to validate an instance
-		// document
-		Validator validator = schema.newValidator();
-
-		// validate the DOM tree
-		thrown.expect(SAXException.class);
-		try {
-			validator.validate(new DOMSource(document));
-		} catch (SAXException e) {
-			e.printStackTrace();
-		}
-
-	}
 }
